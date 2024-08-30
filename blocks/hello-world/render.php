69488e5380e5b61dbaef583e54a69a706f4ff98b
Jesus Carrero <jesusenrique.carrero@gmail.com>
2024-08-30 17:57:01 -0400

    Implementing advance classes from Gutenberg

--- blocks/hello-world/render.php
+++ blocks/hello-world/render.php
@@ -1,9 +1,11 @@
 <?php
 $data = get_field('data');
 $blockID = $block['id'];
+$advanceClasses = isset($block['className']) ? $block['className'] : '';
 ?>
-<div class="hello-world" id="<?= $blockID ?>">
+
+<div class="hello-world <?= esc_attr($advanceClasses); ?>" id="<?= esc_attr($blockID); ?>">
   <h1>Render block</h1>
   <p>This is the render block</p>
-  <p><?= $data['text'] ?></p>
+  <p><?= esc_html($data['text'] ?? ""); ?></p>
 </div>
