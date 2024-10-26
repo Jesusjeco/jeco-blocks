07296797e51ab07615a9a020523ebfbb6656a18b
Jesus Carrero <jesusenrique.carrero@gmail.com>
2024-10-25 21:14:58 -0400

    Finish footer

--- blocks/yva-footer/render.php
+++ blocks/yva-footer/render.php
@@ -6,5 +6,30 @@ $advanceClasses = isset($block['className']) ? $block['className'] : '';
 ?>
 
 <div class="yva-footer <?= esc_attr($advanceClasses); ?>" id="<?= esc_attr($blockID); ?>">
-  <h1>yva-footer</h1>
+  <div class="data">
+    <div class="left">
+      <?= $data['content_1'] ?>
+    </div>
+    <div class="right">
+      <div class="icons">
+        <?php
+        $icons = isset($data['icons']) ? $data['icons'] : null;
+        if ($icons) {
+          foreach ($icons as $key => $icon) { ?>
+            <div class="icon">
+              <a target="_blank" href="<?= $icon['url'] ?>">
+                <img src="<?= $icon['image']['url'] ?>" alt="<?= $icon['image']['alt'] ?>">
+              </a>
+            </div>
+        <?php
+          }
+        }
+        ?>
+      </div>
+      <?= $data['content_2'] ?>
+    </div>
+  </div>
+  <div class="logo">
+    <img loading="lazy" src="<?= $data['logo']['url'] ?>" alt="<?= $data['logo']['alt'] ?>">
+  </div>
 </div>
